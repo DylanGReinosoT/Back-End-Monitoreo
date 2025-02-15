6d75e44ef302d877bf41cf4778cd3155bae7c241
DylanGReinosoT <dgreinoso@espe.edu.ec>
2025-02-15 11:37:07 -0500

    back

---
+++ Mongo/Prototipo/src/main/java/com/example/Prototipo/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.example.Prototipo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOriginPatterns("https://emoti-learn-ai.vercel.app/", "http://localhost:3000/", "http://localhost")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
